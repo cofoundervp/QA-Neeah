14960457a85d820d26597c595f25046e9ae17356
Tu Duong <tu.duong@cofoundervp.com>
2016-10-05 14:20:38 +0700

    Update run basic smoke test suite.bat

--- Cofiddle - Smoke Test Web Client/run basic smoke test suite.bat
+++ Cofiddle - Smoke Test Web Client/run basic smoke test suite.bat
@@ -1,4 +1,4 @@
 @echo OFF
 set myfolder=%date:~-4%%date:~4,2%%date:~7,2%%time:~0,2%%time:~3,2%%time:~6,2%
-call pybot --outputdir "%~dp0\Report" "E:\Cofiddle RUN\Basic Smoke Test Suite"
+call pybot --outputdir "%~dp0\Report" "E:\4.GitHub\QA-Neeah\Cofiddle - Smoke Test Web Client"
 
